7eae1c7b66e9eb3dba735c937aab22aad45bd4bc
Carlos Moreno Serrano <carlosmoreno@meta.com>
2023-06-23 20:10:47 -0700

    Styled About component

--- student-store-ui/src/components/About/About.jsx
+++ student-store-ui/src/components/About/About.jsx
@@ -1,10 +1,24 @@
 import * as React from "react"
 import "./About.css"
+import Logo from "../Logo/Logo"
 
 export default function About() {
   return (
     <div className="about">
-        <p>About</p>
+        <h2>About</h2>
+
+
+        <div className="box">
+          <div className="text">
+            <p>The codepath student store offers great products at great prices from a great team and for a great cause.</p>
+            <p>We've searched far and wide for items that perk the interests of even the most eccentric students and decided to offer them all here in one place.</p>
+            <p>All proceeds go towards bringing high quality CS education to college students around the country.</p>
+          </div>
+
+          <div className="logo">
+            <img src="https://codepath-student-store-demo.surge.sh/assets/codepath.f1b3e41a.svg" alt="codepath logo"></img>
+          </div>
+        </div>
     </div>
   )
 }
