1d72206fa3f62ea09c34bab7437b1c3bc0909193
Carlos Moreno Serrano <carlosmoreno@meta.com>
2023-06-30 17:40:55 -0700

    Added some css styling to the shopping cart and checkout form.

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -9,21 +9,38 @@ export default function ShoppingCart({cartedItems, displayCartedItem, tax, total
 
         {
           Object.keys(cartedItems).length > 0 ? (
-
             <>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Item Name</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.keys(cartedItems).map((id) => {
+                    const item = displayCartedItem(id);
+                    const name = item[0]
+                    const quantity = item[1]
+                    const subtotal = item[2]
+                    return (
+                      <tr key={id}>
+                        <td>{name}</td>
+                        <td>{quantity}</td>
+                        <td>${subtotal}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
 
-            {Object.keys(cartedItems).map( (id) => 
-            (<div key={id}>
-              {displayCartedItem(id).join(" ")}
-            </div>))}
-
-            <div>{tax.toFixed(2)}</div>
-
-            <div>{total.toFixed(2)}</div>
-
+              <div>Tax: ${tax.toFixed(2)}</div>
+              <div>Total: ${total.toFixed(2)}</div>
             </>
-            
-          ) : (<div>No items in cart</div>)
+          ) : (
+            <div>No items in cart</div>
+          )
         }
     </div>
   )
